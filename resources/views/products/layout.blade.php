03d50f8046fd2c44175e979da97eb317442d8c88
unknown <Administrator@DESKTOP-U0OKO98.msi.com>
2023-02-20 12:55:40 +0800

    :wq

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel CRUD Application</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>CRUD WEB Application</h2>
+            </div>
+         
+        </div>
+    </div>
+  
+      
+    
+    <div class="container">
+        <br>
+        @yield('content')
+    </div>
+
+</body>
+</html>
